b54bcd53acf80a518c806c675d50de87df670923
Sajan <sajanmhatre29@gmail.com>
2021-09-30 16:25:12 +0530

    Throw Exception test case 2

--- src/test/java/censusanalyser/CensusAnalyserTest.java
+++ src/test/java/censusanalyser/CensusAnalyserTest.java
@@ -16,8 +16,23 @@ public class CensusAnalyserTest {
             CensusAnalyser censusAnalyser = new CensusAnalyser();
             int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
             Assert.assertEquals(29,numOfRecords);
+            System.out.println("Test Case 1 Pass");
         } catch (CensusAnalyserException e) {
             e.printStackTrace();
         }
     }
+    //Test Case-2 Given the State Census CSV File if incorrect Returns a custom Exception
+    @Test
+    public void givenIndiaCensusData_WithWrongFile_ShouldThrowException() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_PATH);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
+            System.out.println("Test Case 2 Pass");
+        }
+    }
+
 }
