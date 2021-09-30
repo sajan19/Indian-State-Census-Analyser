0472f0ee87d7fd903c125a09f74214f3c2848d03
Sajan <sajanmhatre29@gmail.com>
2021-09-30 23:13:49 +0530

    Type incorrect returns Exception

--- src/test/java/censusanalyser/CensusAnalyserTest.java
+++ src/test/java/censusanalyser/CensusAnalyserTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 public class CensusAnalyserTest {
-    //Decalre the file Path using  a String
+    //Declare the file Path using a String
     private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
     private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
     //Test Case 1 --> Given the States Census CSV file, Check to ensure the Number of Record matches
@@ -34,5 +34,19 @@ public class CensusAnalyserTest {
             System.out.println("Test Case 2 Pass");
         }
     }
-
+    /* Test Case-3 Given the State Census CSV File
+    when correct but type incorrect
+    Returns a custom Exception */
+    @Test
+    public void givenIndiaCensusData_WithWrongFileType_ShouldThrowException() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_PATH);
+        } catch (CensusAnalyserException e) {
+            System.out.println("Test Case 3 Pass");
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
+        }
+    }
 }
